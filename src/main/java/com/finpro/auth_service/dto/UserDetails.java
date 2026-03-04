706555b9c57ba2d5ad2653005c2b1cf360f55eb5
Roronoa-Zoro-007 <strawhats.rz@gmail.com>
2026-03-04 19:41:07 +0530

    User Profile Phase 2

--- src/main/java/com/finpro/auth_service/dto/UserDetails.java
+++ src/main/java/com/finpro/auth_service/dto/UserDetails.java
@@ -1,6 +1,22 @@
 package com.finpro.auth_service.dto;
 
+import java.time.LocalDate;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 public class UserDetails {
-
+	private Long userId;
+	private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String country;
+    private String pincode;
+    private LocalDate dateOfBirth;
+    private String profileImageUrl; 
 }
--- src/main/java/com/finpro/auth_service/service/UserProfileService.java
+++ src/main/java/com/finpro/auth_service/service/UserProfileService.java
@@ -3,14 +3,18 @@ package com.finpro.auth_service.service;
 import org.springframework.http.ResponseEntity;
 
 import com.finpro.auth_service.dto.UserDetails;
+import com.finpro.auth_service.entity.User;
 import com.finpro.auth_service.entity.UserProfile;
 import com.finpro.auth_service.repository.UserProfileRepository;
+import com.finpro.auth_service.repository.UserRepository;
 
 public class UserProfileService {
 	private final UserProfileRepository userProfileRepo ;
+	private final UserRepository userRepo;
 	
-	public UserProfileService (UserProfileRepository userProfileRepo) {
+	public UserProfileService (UserProfileRepository userProfileRepo, UserRepository userRepo) {
 		this.userProfileRepo = userProfileRepo;	
+		this.userRepo = userRepo;
 	}
 
 	public UserDetails getUserProfile(Long userId) {
@@ -21,6 +25,18 @@ public class UserProfileService {
 	public ResponseEntity<?> createUserProfile(UserDetails userDetails) {
 		
 		UserProfile newProfile = new UserProfile();
+		User user = userRepo.findById(userDetails.getUserId())
+				.orElseThrow(() -> new RuntimeException("User Not Found"));
+				
+		newProfile.setUser(user);
+		newProfile.setAddress(userDetails.getAddress());
+		newProfile.setCity(userDetails.getCity());
+		newProfile.setCountry(userDetails.getCountry());
+		newProfile.setFirstName(userDetails.getFirstName());
+		newProfile.setLastName(userDetails.getLastName());
+		newProfile.setPincode(userDetails.getPincode());
+		newProfile.setDateOfBirth(userDetails.getDateOfBirth());
+		newProfile.setProfileImageUrl(userDetails.getProfileImageUrl());
 		
 		return ResponseEntity.ok(userProfileRepo.save(newProfile));
 	}
